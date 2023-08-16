f0df7f0b554e32dc3722f81bfa867bba4c9b072e
Veeresha K <vkzoomin996@gmail.com>
2023-08-16 21:21:52 +0530

    created branch

--- nishan/src/test/java/nishan/DeletedataTest.java
+++ nishan/src/test/java/nishan/DeletedataTest.java
@@ -7,5 +7,8 @@ public class DeletedataTest {
 	@Test
 	public void delete() {
 		System.out.println("--- delete data---");
+	}@Test
+	public void delete1() {
+		System.out.println("--- delete data---");
 	}
 }
