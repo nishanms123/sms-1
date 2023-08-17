db0e7f594a154ecbd21f7428375b8ebe5524e468
Veeresha K <vkzoomin996@gmail.com>
2023-08-17 11:02:38 +0530

    go through this commit

---
+++ nishan/src/test/java/nishan/DeletedataTest2.java
@@ -0,0 +1,19 @@
+package nishan;
+
+import org.testng.annotations.Test;
+
+public class DeletedataTest {
+
+	@Test
+	public void delete() {
+		System.out.println("--- delete data---");
+	}@Test
+	public void delete1() {
+		System.out.println("--- delete data---");
+	}
+	@Test
+	public void restore()
+	{
+		System.out.println("----restored----");
+	}
+}
