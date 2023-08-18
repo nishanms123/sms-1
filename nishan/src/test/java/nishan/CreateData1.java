dc868879ebcb764fa672c382f1bec68a22f828a7
nishan123 <msnishan66@gmail.com>
2023-08-18 13:27:08 +0530

    new commit

--- nishan/src/test/java/nishan/CreateData1.java
+++ nishan/src/test/java/nishan/CreateData1.java
@@ -1,24 +1,15 @@
 package nishan;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+
+
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 
 public class CreateData1 {
 	
-	@Test(groups = "smoke")
-	public void createData()
-	{
-		WebDriverManager.edgedriver().setup();
-		WebDriver driver=new EdgeDriver();
-		EdgeOptions option=new EdgeOptions();
-		option.addArguments("--disable--notifications");
-		System.out.println("--create data---");
-	}
+	
 	@Test(groups = "smoke")
 	public void deleteData()
 	{
